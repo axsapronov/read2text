36c464e85d72ed76627265d86d376570b892ba9c
warmonger <d>
2014-12-29 13:06:38 +0600

    Init

---
+++ read2text/readability/__init__.py
@@ -0,0 +1,2 @@
+from read2text.readability.readability import Document, main
+from read2text.readability.page_parser import ascii, Unparseable
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='read2text',
+    version='0.0.1',
+    url='',
+    license='',
+    packages=find_packages(),
+    author='Brett Terpstra',
+    author_email='',
+    description='read2text',
+    include_package_data=True,
+    install_requires=[
+    ],
+)
